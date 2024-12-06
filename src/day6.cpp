55718116a5cd7560c0143379ad716419e941880f
Louis-Guillaume Gagnon <gagnonlg@protonmail.com>
2024-12-06 18:30:41 +0100

    Day 6, Part 2

--- src/day6.cpp
+++ src/day6.cpp
@@ -1,3 +1,4 @@
+#include <set>
 #include <unordered_set>
 
 #include "common.hpp"
@@ -5,21 +6,21 @@
 namespace day6 {
 
 struct InputData {
-    std::unordered_set<int64_t> obstacles;
-    std::unordered_set<int64_t> visited;
+    std::set<std::pair<int64_t,int64_t>> obstacles;
+    std::set<std::pair<int64_t,int64_t>> visited;
     std::pair<int64_t, int64_t> startpos;
     std::pair<int64_t, int64_t> bounds;
     
     void add_obstacle(size_t i, size_t j) {
-	obstacles.insert(ans(i) * bounds.second + ans(j));
+	obstacles.insert(std::make_pair(ans(i), ans(j)));
     }
 
     int64_t count_visited(int64_t i, int64_t j) {
-	return visited.insert(i * bounds.second + j).second ? 1 : 0;
+	return visited.insert(std::make_pair(i, j)).second ? 1 : 0;
     }
 
     bool obstacle_at(int64_t i, int64_t j) const {
-	return obstacles.contains(i * bounds.second + j);
+	return obstacles.contains({i, j});
     }
 
     bool out_of_bounds(int64_t i, int64_t j) const {
@@ -56,17 +57,16 @@ void rotate_right(int64_t& i, int64_t& j)
     j = -tmp;
 }
 
-Answer part_1(const std::string& input)
+std::vector<std::pair<int64_t, int64_t>> trace_path(const InputData& data)
 {
-    InputData data = read_input_data(input);
-    auto [i, j] = data.startpos;
+    std::vector<std::pair<int64_t, int64_t>> trace;
 
+    auto [i, j] = data.startpos;
     int64_t i_dir = -1;
     int64_t j_dir = 0;
     
-    Answer dist = 0;
     while (!data.out_of_bounds(i, j)) {
-	dist += data.count_visited(i, j);
+	trace.emplace_back(i, j);
 
 	int64_t i_next = i + i_dir;
 	int64_t j_next = j + j_dir;
@@ -79,12 +79,61 @@ Answer part_1(const std::string& input)
 	j = j_next;
     }
 
-    return dist;
+    return trace;
 }
 
-Answer part_2(const std::string& /*input*/)
+bool has_cycle_with_obs(const InputData& data, int64_t i_obs, int64_t j_obs)
 {
-    throw NotImplemented();
+    auto [i, j] = data.startpos;
+    int64_t i_dir = -1;
+    int64_t j_dir = 0;
+
+    if (i == i_obs && j == j_obs) {
+	return false;
+    }
+
+    std::set<std::tuple<int64_t,int64_t,int64_t,int64_t>> seen;
+    
+    while (!data.out_of_bounds(i, j)) {
+	// Have I already seen this one?
+	if (!seen.insert({i, j, i_dir, j_dir}).second) {
+	    // Le cycle!
+	    return true;
+	}
+	int64_t i_next = i + i_dir;
+	int64_t j_next = j + j_dir;
+	while (data.obstacle_at(i_next, j_next) ||
+	       (i_next == i_obs && j_next == j_obs)) {
+	    rotate_right(i_dir, j_dir);
+	    i_next = i + i_dir;
+	    j_next = j + j_dir;
+	}
+	i = i_next;
+	j = j_next;
+    }
+
+    return false;
+}
+
+
+Answer part_1(const std::string& input)
+{
+    InputData data = read_input_data(input);
+    std::vector<std::pair<int64_t, int64_t>> trace = trace_path(data);
+    std::sort(trace.begin(), trace.end());
+    return std::unique(trace.begin(), trace.end()) - trace.begin();
+}
+
+Answer part_2(const std::string& input)
+{
+    InputData data = read_input_data(input);
+    std::vector<std::pair<int64_t, int64_t>> trace = trace_path(data);
+    std::sort(trace.begin(), trace.end());
+    auto it = std::unique(trace.begin(), trace.end());
+    trace.resize(static_cast<size_t>(std::distance(trace.begin(), it)));
+    return std::count_if(trace.begin(), trace.end(), [&](const auto& pos){
+	return has_cycle_with_obs(data, pos.first, pos.second);
+    });
 }
 
 void tests()
@@ -126,6 +175,19 @@ void tests()
 
     CHECK(part_1(test_input_1) == 41);
 
+    /////////////////////////////////////////////////////
+     
+#define CHECK_CYCLE(i, j) CHECK(has_cycle_with_obs(data, i, j))  
+    CHECK_CYCLE(6, 3);
+    CHECK_CYCLE(7, 6);
+    CHECK_CYCLE(7, 7);
+    CHECK_CYCLE(8, 1);
+    CHECK_CYCLE(8, 3);
+    CHECK_CYCLE(9, 7);
+
+    CHECK(part_2(test_input_1) == 6);
+
+    
 }
 
 } //namespace day6
